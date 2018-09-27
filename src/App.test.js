3403a2761c16163ad036f362984f4f6c5c995cfe
budougumi0617 <budougumi0617@gmail.com>
2018-09-27 20:18:32 +0900

    :+1: Add test file, and setupTests.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow  } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const emptyProps ={
+  text: 'test'
+};
+
+describe('App', () => {
+  describe('App-title', () => {
+    const cases = [
+      ['simple test1', { text: 'Jest'}, 'Welecome to Jest'],
+      ['simple test2', { text: 'enzyme'}, 'welcome to enzyme']
+    ];
+    describe.each(cases)('change Props', (title, override, expected) => {
+       it(title, () => {
+         const customProps = Object.assign({}, emptyProps, override);
+         const wrapper = shallow(<App {...customProps} />);
+         expect(wrapper.instance().props.text).toBe(customProps.text);
+         expect(wrapper.find('.App-title').text()).toBe(expected);
+       });
+    });
+  });
 });
